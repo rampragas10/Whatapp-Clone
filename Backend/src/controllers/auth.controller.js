971ad0e9af758ccf46cd6be7e9ac5e72ee968777
rampragas10 <rampragas10@gmail.com>
2026-05-14 00:28:25 +0545

    Added updateProfile route and Cloudaniry for storage

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -83,7 +83,7 @@ export const login = async (req, res) => {
     if (!user) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
-    
+
 
     // Check password
     const isMatch = await bcrypt.compare(password, user.password);
@@ -98,5 +98,33 @@ export const login = async (req, res) => {
   } catch (error) {   
     res.status(500).json({ message: error.message });
     }
+};
+
+export const logout = (_, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.status(200).json({ message: "Logged out successfully" });
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    if (!profilePic)
+      return res.status(400).json({ message: "Profile pic is required" });
+
+    const userId = req.user._id;
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true },
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("Error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { register,login } from "../controllers/auth.controller.js";
+import { register,login ,logout,updateProfile} from "../controllers/auth.controller.js";
+import { authUser } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -10,6 +11,23 @@ router.post("/login", login);
 // @route   POST api/auth/register
 // @desc    Register user and return JWT token
 router.post("/register", register);
+
+// @route   POST api/auth/logout
+// @desc    Logout user
+router.post("/logout", logout);
+
+// @route   PUT api/auth/profile
+// @desc    Update user profile
+router.put("/profile", authUser, updateProfile);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+// @route   GET api/auth/check
+// @desc    Check if user is authenticated
+router.get("/check", protectRoute, (req, res) =>
+  res.status(200).json(req.user),
+);
+
 
 
 export default router;
