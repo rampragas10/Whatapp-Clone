b8815c6ed3acdd771859681b50c469242e833a50
rampragas10 <rampragas10@gmail.com>
2026-05-15 22:23:20 +0545

    Solve minor problem

--- Backend/src/middleware/socket.auth.middleware.js
+++ Backend/src/middleware/socket.auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-// import User from "../models/User.model.js";
+import User from "../models/user.model.js";
 import {config} from "../config/config.js";
 
 export const socketAuthMiddleware = async (socket, next) => {
